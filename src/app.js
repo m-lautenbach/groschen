3c3ca370ebbc62725706efed6efebba157c8a1b6
Markus Lautenbach <markus.lautenbach@gmail.com>
2017-07-04 20:38:18 +0200

    Add file input

--- src/app.js
+++ src/app.js
@@ -1,38 +1,35 @@
 import xs from 'xstream';
-import {run} from '@cycle/run';
-import {div, input, h2, makeDOMDriver} from '@cycle/dom';
+import { run } from '@cycle/run';
+import { makeDOMDriver } from '@cycle/dom';
+import { html } from 'snabbdom-jsx'
 
 function main(sources) {
-  const changeWeight$ = sources.DOM.select('.weight')
-    .events('input')
-    .map(ev => ev.target.value);
+  const files$ = sources.DOM
+    .select('.csv-input')
+    .events('change')
+    .map(evt => evt.target.files)
+    .startWith([])
 
-  const changeHeight$ = sources.DOM.select('.height')
-    .events('input')
-    .map(ev => ev.target.value);
+  const vdom$ = files$
+    .map(files => {
+      const file = files[0]
+      const stream = xs.create()
+      if (file) {
+        var reader = new FileReader();
 
-  const weight$ = changeWeight$.startWith(70);
-  const height$ = changeHeight$.startWith(170);
+        reader.onload = evt => stream.shamefullySendNext(evt.target.result)
 
-  const bmi$ = xs.combine(weight$, height$)
-    .map(([weight, height]) => {
-      const heightMeters = height * 0.01;
-      return Math.round(weight / (heightMeters * heightMeters));
-    });
-
-  const vdom$ = bmi$.map(bmi =>
-    div([
-      div([
-        'Weight ___kg',
-        input('.weight', {attrs: {type: 'range', min: 40, max: 140}})
-      ]),
-      div([
-        'Height ___cm',
-        input('.height', {attrs: {type: 'range', min: 140, max: 210}})
-      ]),
-      h2('BMI is ' + bmi)
-    ])
-  );
+        reader.readAsText(file)
+      }
+      return stream
+        .map(fileContent => <div>{fileContent}</div>)
+        .startWith(null)
+        .map(fileElement => <div>
+          <input type="file" className="csv-input" />
+          {fileElement || ''}
+        </div>)
+    })
+    .flatten()
 
   return {
     DOM: vdom$
